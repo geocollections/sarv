42c8a4ccae4de8ee424aaed6f0e9541e6f0c6a7a
Sander <sandertint@gmail.com>
2015-12-31 14:27:01 +0200

    Menu data and structure update

--- sarv/installation/sarv_data.sql
+++ sarv/installation/sarv_data.sql
@@ -23,13 +23,37 @@ CREATE DATABASE IF NOT EXISTS `sarv` DEFAULT CHARACTER SET latin1 COLLATE latin1
 USE `sarv`;
 
 --
--- Dumping data for table `sarv_acl`
+-- Dumping data for table `sarv_database`
 --
 
-INSERT INTO `sarv_acl` (`id`, `id_tested`, `type`, `id_destination_row`, `id_destination`, `id_permission`, `rights_group_id`, `destination_type`) VALUES
-(1, 1, 'group', 1, NULL, 1, 5, 17),
-(2, 1, 'group', 2, NULL, 1, 5, 17),
-(3, 1, 'group', 3, NULL, 1, 5, 17);
+INSERT INTO `sarv_database` (`id`, `acronym`, `name`, `name_en`) VALUES
+(1, 'tes', 'test', '');
+
+
+--
+-- Dumping data for table `sarv_page`
+--
+
+INSERT INTO `sarv_page` (`id`, `name`, `url`, `language`, `visibility`, `settings`) VALUES
+(1, 'Administration', '', 'en-us', 'acl', NULL),
+(2, 'Pages administration', 'admin/menu', 'en-us', 'acl', NULL),
+(3, 'User right administration', 'admin/acl', 'en-us', 'acl', NULL);
+
+--
+-- Dumping data for table `sarv_menu`
+--
+
+INSERT INTO `sarv_menu` (`id`, `column`, `row`, `page_id`, `usergroup_id`) VALUES
+(1, 1, 0, 1, NULL),
+(2, 1, 1, 2, NULL),
+(3, 1, 2, 3, NULL);
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `forename`, `surename`, `email`, `remarks`, `isikukood`, `priv`, `dbs`, `db`, `user_added`, `date_added`, `user_changed`, `date_changed`, `timestamp`, `database_id`) VALUES
+(1, 'admin', '', '', '', '', 0, NULL, '', 'tes', 'admin', '2015-12-31 13:17:15', '', '2015-12-31 13:17:15', '2015-12-31 13:17:15', 1);
 
 --
 -- Dumping data for table `sarv_acl_group`
@@ -67,36 +91,13 @@ INSERT INTO `sarv_acl_user_group` (`id`, `id_group`, `id_user`) VALUES
 (1, 1, 1);
 
 --
--- Dumping data for table `sarv_database`
+-- Dumping data for table `sarv_acl`
 --
 
-INSERT INTO `sarv_database` (`id`, `acronym`, `name`, `name_en`) VALUES
-(1, 'tes', 'test', '');
-
---
--- Dumping data for table `sarv_menu`
---
-
-INSERT INTO `sarv_menu` (`id`, `column`, `row`, `page_id`, `usergroup_id`) VALUES
-(1, 1, 0, 1, NULL),
-(2, 1, 1, 2, NULL),
-(3, 1, 2, 3, NULL);
-
---
--- Dumping data for table `sarv_page`
---
-
-INSERT INTO `sarv_page` (`id`, `name`, `url`, `language`, `visibility`, `settings`) VALUES
-(1, 'Administration', '', 'en-us', 'acl', NULL),
-(2, 'Pages administration', 'admin/menu', 'en-us', 'acl', NULL),
-(3, 'User right administration', 'admin/acl', 'en-us', 'acl', NULL);
-
---
--- Dumping data for table `user`
---
-
-INSERT INTO `user` (`id`, `username`, `forename`, `surename`, `email`, `remarks`, `isikukood`, `priv`, `dbs`, `db`, `user_added`, `date_added`, `user_changed`, `date_changed`, `timestamp`, `database_id`) VALUES
-(1, 'admin', '', '', '', '', 0, NULL, '', 'tes', 'admin', '2015-12-31 13:17:15', '', '2015-12-31 13:17:15', '2015-12-31 13:17:15', 1);
+INSERT INTO `sarv_acl` (`id`, `id_tested`, `type`, `id_destination_row`, `id_destination`, `id_permission`, `rights_group_id`, `destination_type`) VALUES
+(1, 1, 'group', 1, NULL, 1, 5, 17),
+(2, 1, 'group', 2, NULL, 1, 5, 17),
+(3, 1, 'group', 3, NULL, 1, 5, 17);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
